9a0275026efeabd4294af878680262772563b570
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-12-26 09:54:46 -0300

    Modify: modal for invite players

--- src/assets/components/InvitePlayerModal.jsx
+++ src/assets/components/InvitePlayerModal.jsx
@@ -11,7 +11,11 @@ const InvitePlayerModal = ({ closeModal }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black/75">
       <div className="bg-grey-hextech-black w-1/4 h-3/4 border border-gold-4 p-4">
         <form className="flex items-center gap-2">
-          <input type="text" className="w-full flex-1 h-7 border border-gold-4" />
+          <input
+            type="text"
+            placeholder="Escribe un nombre para invitar"
+            className="w-full flex-1 h-7 text-xs text-gold-1 pl-2 bg-black border border-gold-4 focus:border-gold-1 focus:outline-none placeholder:font-spiegel placeholder:text-grey-1 placeholder:pl-1"
+          />
           <Button
             desc="Add players"
             size={8}
